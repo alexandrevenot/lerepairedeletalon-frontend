773de70e2268365d3206c55b6eaf831bc1f5c3e9
Alexandre Venot <venotalexandre@gmail.com>
2024-05-21 17:02:43 +0200

    Added cookies notification

---
+++ src/app/core/cookies/cookies.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class CookiesService {
+
+  acceptCookies() {
+    localStorage.setItem(`cookiesAreAccepted`, 'yes');
+  }
+
+  cookiesAreAccepted() {
+    let value: string | null = localStorage.getItem(`cookiesAreAccepted`);
+    console.log(value)
+    return value? true: false;
+  }
+}
--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CookiesService } from 'src/app/core/cookies/cookies.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  providers: [CookiesService]
 })
-export class FooterComponent {
-  constructor(private router: Router) {}
+export class FooterComponent implements OnInit {
+  public cookiesAreAccepted: boolean = false;
+
+  constructor(
+    private router: Router,
+    private cookiesService: CookiesService
+  ) {}
+
+  ngOnInit(): void {
+    this.cookiesAreAccepted = this.cookiesService.cookiesAreAccepted();
+  }
+
+  acceptCookies() {
+    this.cookiesService.acceptCookies();
+    this.cookiesAreAccepted = true;
+  }
 
   navigateTo(path: string) {
     this.router.navigate([`/${path}`]);
